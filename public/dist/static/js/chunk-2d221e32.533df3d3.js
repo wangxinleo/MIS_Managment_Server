c41059cadb55832f6f2cb0537b7e126a4bf6acff
wangxinleo <wangxin.leo@outlook.com>
2020-06-13 14:26:31 +0800

    修改bug

--- public/dist/static/js/chunk-2d221e32.533df3d3.js
+++ public/dist/static/js/chunk-2d221e32.533df3d3.js
@@ -1,6 +1,6 @@
 /*!
  *  基于vue构建  
  *  copyright: wangxinleo wangxin.leo@outlook.com 
- *  time: 2020-6-12 11:14:32
+ *  time: 2020-6-13 13:39:43
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d221e32"],{cbeb:function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"MISPCMaintain-container"},[n._v(" 正在开发，下一版本放出 ")])},c=[],i={name:"MispcMaintain",components:{},data:function(){return{}},created:function(){},mounted:function(){},methods:{}},o=i,u=e("2877"),s=Object(u["a"])(o,a,c,!1,null,null,null);t["default"]=s.exports}}]);
--- public/dist/static/js/chunk-2d230fe7.6959fd35.js
+++ public/dist/static/js/chunk-2d230fe7.6959fd35.js
@@ -1,6 +1,6 @@
 /*!
  *  基于vue构建  
  *  copyright: wangxinleo wangxin.leo@outlook.com 
- *  time: 2020-6-12 11:14:32
+ *  time: 2020-6-13 13:39:43
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,r,n){"use strict";n.r(r);n("ac1f"),n("5319");var t,u,a={name:"Redirect",created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("2877"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
