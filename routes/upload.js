633cb812bc77500ddccd15ad0e4aac846bc8619b
wangxinleo <wangxin.leo@outlook.com>
2020-08-07 17:32:21 +0800

    更新文件档案

--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 const multer = require('multer')
+const request = require('request')
+const app = require('../app')
 
 router.post(
   '/MISFile',
@@ -12,6 +14,12 @@ router.post(
     let file = req.file
     //这里修改文件名字，比较随意
     fs.renameSync('./files/' + file.filename, './files/' + file.originalname)
+    request(
+      { url: 'http://127.0.0.1:8099/api/MIS/reloadFiles', method: 'get' },
+      (res) => {
+        console.log(res)
+      }
+    )
     return res.json({
       code: 200,
       msg: '上传成功!',
