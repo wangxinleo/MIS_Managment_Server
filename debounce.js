d19784d4efc0698f5d83f0c8171b2dce0a1ebd55
wangxinleo <wangxin.leo@outlook.com>
2021-06-14 18:14:25 +0800

    update 1814

--- debounce.js
+++ debounce.js
@@ -1,33 +1,45 @@
 function debounce (func, wait, immediate) {
-	let timer;
-
+	let timer
 	return function () {
-		let that = this;
-		let args = arguments;
-
-		if (timer) clearTimeout(timer);
-		if (immediate) {
-			var callNow = !timer;
-			timer = setTimeout(() => {
-				timer = null;
-			}, wait);
-			if (callNow) func.apply(that, args);
-		} else {
+		const that  = this
+		const args = arguments
+		if(timer) clearTimeout(timer)
+		if(immediate){
+			let callNow = !timer;
 			timer = setTimeout(function () {
-				func.apply(that, args);
-			}, wait);
+				timer = null
+			},wait)
+			if(callNow) func.apply(that, args)
+		}else {
+			timer = setTimeout(function () {
+				func.apply(that,args)
+			},wait)
 		}
-	};
+
+	}
 }
 
-debounce(res => {
+const a = debounce(res => {
 	console.log(res);
-}, 5000, false)(3);
+}, 2000, true);
 
-debounce(res => {
-	console.log(res);
-}, 5000, true)(3);
+setTimeout(function (){
+	a(3)
+},1000)
+setTimeout(function (){
+	a(2)
+},1000)
+setTimeout(function (){
+	a(1)
+},1000)
+setTimeout(function (){
+	a(0)
+},1000)
 
-debounce(res => {
-	console.log(res);
-}, 5000, true)(3);
+// debounce(res => {
+// 	console.log(res);
+// }, 2000, true)(3);
+//
+// debounce(res => {
+// 	console.log(res);
+// }, 3000, true)(3);
